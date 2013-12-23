699cc60f063249f7067ae8a6593ea2ce7f64ea94
Domikado 3 <domikado3@Domikado-3.local>
2013-12-23 13:08:29 +0700

    first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Celengin::Application.config.secret_key_base = 'e8242a9e539ea481f9ec54f2eb3e7358b903ddd8773ca206822b07f63286d453b689b678f4690555adcb66ca485d032b1fb3e3fd93b99a0cf153b559f07fa289'
